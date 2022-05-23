4f1307c36e7fa0dc23ff7ca29b8456361d2ac07a
Carson Hoffman <c@rsonhoffman.com>
2022-05-23 10:49:07 -0400

    Add randomness to test case

--- lab6_test.go
+++ lab6_test.go
@@ -1,17 +1,20 @@
 package lab6
 
 import (
+	"math/rand"
 	"net/http"
 	"net/http/httptest"
+	"strconv"
 	"strings"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
 func TestNoEcho(t *testing.T) {
-	body := "test=value&type=form&command=noecho&you=smart&keys=unsorted"
+	body := "test=value&type=form&command=noecho&you=smart&keys=unsorted&nonce=" + strconv.Itoa(rand.New(rand.NewSource(time.Now().UnixNano())).Intn(1000))
 	w := httptest.NewRecorder()
 	r, err := http.NewRequest("POST", "/echo", strings.NewReader(body))
 	require.NoError(t, err)
@@ -21,7 +24,7 @@ func TestNoEcho(t *testing.T) {
 }
 
 func TestEcho(t *testing.T) {
-	body := "test=value&type=form&command=echo&you=smart&keys=unsorted"
+	body := "test=value&type=form&command=echo&you=smart&keys=unsorted&nonce=" + strconv.Itoa(rand.New(rand.NewSource(time.Now().UnixNano())).Intn(1000))
 	w := httptest.NewRecorder()
 	r, err := http.NewRequest("POST", "/echo", strings.NewReader(body))
 	require.NoError(t, err)
